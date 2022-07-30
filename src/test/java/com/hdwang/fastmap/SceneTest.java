a78e9238f9c1ba8fdc33a9c8651bed3539267ddc
hdwang <1182583214@qq.com>
2022-07-30 22:28:42 +0800

    修改案例代码

--- src/test/java/com/hdwang/fastmap/SceneTest.java
+++ src/test/java/com/hdwang/fastmap/SceneTest.java
@@ -1,9 +1,12 @@
 package com.hdwang.fastmap;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-/**、
+/**
+ * 、
  * 场景测试
+ *
  * @author wanghuidong
  * 时间： 2022/6/26 16:14
  */
@@ -15,14 +18,29 @@ public class SceneTest {
         //记录每次访问记录：key:时间戳，value:访问次数
         IFastMap<Long, Long> innerFastMap = new FastMap<>(false, true);
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        innerFastMap.put(dateFormat.parse("2022-06-26 16:00:00").getTime(), 1L);
-        innerFastMap.put(dateFormat.parse("2022-06-26 16:00:30").getTime(), 1L);
-        innerFastMap.put(dateFormat.parse("2022-06-26 16:00:40").getTime(), 1L);
-        innerFastMap.put(dateFormat.parse("2022-06-26 16:01:00").getTime(), 1L);
-        innerFastMap.put(dateFormat.parse("2022-06-26 16:02:00").getTime(), 1L);
-        innerFastMap.put(dateFormat.parse("2022-06-26 16:03:00").getTime(), 1L);
+//        innerFastMap.put(dateFormat.parse("2022-06-26 16:00:00").getTime(), 1L);
+//        innerFastMap.put(dateFormat.parse("2022-06-26 16:00:30").getTime(), 1L);
+//        innerFastMap.put(dateFormat.parse("2022-06-26 16:00:40").getTime(), 1L);
+//        innerFastMap.put(dateFormat.parse("2022-06-26 16:01:00").getTime(), 1L);
+//        innerFastMap.put(dateFormat.parse("2022-06-26 16:02:00").getTime(), 1L);
+//        innerFastMap.put(dateFormat.parse("2022-06-26 16:03:00").getTime(), 1L);
         String phone = "13312340001";
-        fastMap.put(phone, innerFastMap);
+
+        //put原子操作，线程安全，用来设置值
+//        fastMap.put(phone, innerFastMap);
+
+        //compute原子操作，线程安全，可以用来初始化指定key的值和计算指定key新的值
+        fastMap.compute(phone, (k, v) -> {
+            if (v == null) {
+                v = new FastMap<>(false, true);
+            }
+            try {
+                v.put(dateFormat.parse("2022-06-26 16:00:00").getTime(), 1L);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return v;
+        });
         System.out.println("当前时间；" + System.currentTimeMillis());
 
         //过期时间设置5分钟
